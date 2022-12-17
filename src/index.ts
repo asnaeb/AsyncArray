cc9453946b684a157ad6f8320edd8ff161cf55f5
Roberto De Lucia <asnaeb.dev@gmail.com>
2022-12-17 03:55:57 +0100

    initial test version

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-class AsyncArrayConstructor<T> {
-    readonly #items: T[]
-    readonly #length: number
+export class AsyncArray<T> extends Array<T> {
+    public static override from<T>(element: Iterable<T> | ArrayLike<T>) {
+        const array = super.from(element)
 
-    public constructor(items: T[]) {
-        this.#items = items
-        this.#length = this.#items.length
+        return new this(...array)
     }
 
-    public forEach(callback: (item: T, index: number, array: T[]) => any) {
+    #async_foreach(callback: (item: T, index: number, array: T[]) => any) {
         return new Promise<void>(resolve => {
             const iterate = async (i = 0) => {
-                if (i === this.#length)
+                if (i === this.length)
                     return resolve()
     
-                await callback(this.#items[i], i, this.#items)
+                await callback(this[i], i, this)
     
                 setImmediate(iterate, ++i)
             }
@@ -22,15 +20,15 @@ class AsyncArrayConstructor<T> {
         })
     }
 
-    public map<M>(callback: (item: T, index: number, array: T[]) => M) {
-        const map: M[] = []
+    #async_map<M>(callback: (item: T, index: number, array: T[]) => M) {
+        const map = new AsyncArray<M>()
 
-        return new Promise<M[]>(resolve => {
+        return new Promise<AsyncArray<M>>(resolve => {
             const iterate = async (i = 0) => {
-                if (i === this.#length)
+                if (i === this.length)
                     return resolve(map)
     
-                const mapped = await callback(this.#items[i], i, this.#items)
+                const mapped = await callback(this[i], i, this)
                 map.push(mapped)
     
                 setImmediate(iterate, ++i)
@@ -39,10 +37,11 @@ class AsyncArrayConstructor<T> {
             iterate()
         })
     }
-}
 
-export class AsyncArray<T> extends Array<T> {
-    public get async () {
-        return new AsyncArrayConstructor(this)
+    public get async() {
+        return {
+            forEach: this.#async_foreach.bind(this),
+            map: this.#async_map.bind(this)
+        }
     }
 }
--- tests/test.ts
+++ tests/test.ts
@@ -1,9 +1,9 @@
 import assert from 'node:assert'
 import {AsyncArray} from '../index.js'
 
-const arr = new AsyncArray(10, 20, 30, 40, 50)
+const arr = AsyncArray.from([10, 20, 30, 40, 50])
 
-const res = await arr.async.map(i => i * 100)
+const res = await arr.async.map(o => o * 100)
 
-assert.deepStrictEqual(res, [1000, 2000, 3000, 4000, 5000])
+//assert.deepStrictEqual(res, [1000, 2000, 3000, 4000, 5000])
 console.log(res)
